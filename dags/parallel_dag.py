d9073ab33a60c1e4a666b176014cf37a76e685cc
Thiago Heron <thiagoheronavila@gmail.com>
2021-07-03 17:22:08 -0300

    Adicionando DAG para testar paralelismo entre tasks

---
+++ dags/parallel_dag.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+   'start_date': datetime(2020, 1, 1),
+}
+
+with DAG(
+   "parallel_dag",
+   schedule_interval="@daily",
+   default_args=default_args,
+   catchup=False
+) as dag:
+
+    task_1 = BashOperator(
+        task_id="task_1",
+        bash_command='sleep 3'
+    )
+
+    task_2 = BashOperator(
+        task_id="task_2",
+        bash_command='sleep 3'
+    )
+
+    task_3 = BashOperator(
+        task_id="task_3",
+        bash_command='sleep 3'
+    )
+
+    task_4 = BashOperator(
+        task_id="task_4",
+        bash_command='sleep 3'
+    )
+    
+    task_1 >> [task_2, task_3] >> task_4
